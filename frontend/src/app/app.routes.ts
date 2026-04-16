a19ef610e5d637b060ab415e9198c5c41ee5d205
luisferbarragan <luisfer1918650@gmail.com>
2026-04-16 16:44:02 -0600

    carrito con signal, cantidades, total y eliminar productos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Inicio } from './pages/inicio/inicio';
 import { AgregarProducto } from './pages/agregar-producto/agregar-producto';
 import { Contacto } from './pages/contacto/contacto';
 import { Catalogo } from './pages/catalogo/catalogo';
+import { Carrito } from './pages/carrito/carrito';
 import { ProductoDetalle } from './pages/producto-detalle/producto-detalle';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: Inicio },
   { path: 'catalogo', component: Catalogo },
+  { path: 'carrito', component: Carrito },
   { path: 'productos/:id', component: ProductoDetalle },
   { path: 'agregar-producto', component: AgregarProducto },
   { path: 'contacto', component: Contacto }
--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CarritoService } from '../../services/carrito.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [NgIf, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  constructor(public carritoService: CarritoService) {}
 }
