66ac68cfac10ed7c690061d05bd93fe3a89f8a7b
Arturo Picazo <arturo.picazops@gmail.com>
2026-04-15 22:43:32 -0600

    avance al catalogo y estilos base de productos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { AgregarProducto } from './pages/agregar-producto/agregar-producto';
 import { Contacto } from './pages/contacto/contacto';
+import { Catalogo } from './pages/catalogo/catalogo';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: Inicio },
+  { path: 'catalogo', component: Catalogo },
   { path: 'agregar-producto', component: AgregarProducto },
   { path: 'contacto', component: Contacto }
 ];
